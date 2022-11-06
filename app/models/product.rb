9a18d451b155910d3a001c670a38703d29a9cd5c
bossteve1 <stephen.nyabuto@student.moringaschool.com>
2022-11-06 23:52:08 +0300

    completed tasks

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,20 @@
 class Product < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
+
+    def leave_review(user, star_rating, comment)
+        Review.create(star_rating: star_rating, comment: comment, user_id: user_id, product_id: self.id)
+    end
+
+    def print_all_reviews
+        self.reviews.each do |review|
+           puts "Review for #{review.product.name} by #{review.user.name}: #{review.star_rating}. #{review.comment}"
+        end
+   end
+
+
+   def average_rating
+    self.reviews.average(:star_rating).to_f
+   end
     
 end
